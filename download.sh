e024ee1eb1f25e2c8ac9e17d90718cf949fd484b
Chris Seal <c.seal@auckland.ac.nz>
2021-02-18 14:43:58 +1300

    Get list from rsync

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # download.sh - this script uses rsync over an ssh tunnel to copy the files from the remote server to the datacentre
 # Usage ./download.sh PORT_NUMBER where PORT_NUMBER is the port used for the reverse tunnel
-rsync -avhe "ssh -p $1 -i ~/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/eng/* /srv/data/downloads/eng/
-rsync -avhe "ssh -p $1 -i ~/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/ins/* /srv/data/downloads/ins/
-rsync -avhe "ssh -p $1 -i ~/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/ngrx/* /srv/data/downloads/ngrx/
-rsync -avhe "ssh -p $1 -i ~/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/ngrx_raw/* /srv/data/downloads/ngrx_raw/
+rsync -avhe --output-format="%n" "ssh -p $1 -i ~/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/eng/* /srv/data/downloads/eng/ --dry-run >> test.txt
+#rsync -avhe "ssh -p $1 -i ~/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/ins/* /srv/data/downloads/ins/
+#rsync -avhe "ssh -p $1 -i ~/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/ngrx/* /srv/data/downloads/ngrx/
+#rsync -avhe "ssh -p $1 -i ~/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/ngrx_raw/* /srv/data/downloads/ngrx_raw/
 
