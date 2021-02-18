42729d597948f24e6f46e5544576c521e7cd8a3e
SPOC-sftp <c.seal@auckland.ac.nz>
2021-02-18 15:31:26 +1300

    fixed nc polling

--- poll.sh
+++ poll.sh
@@ -2,5 +2,7 @@
 # Using netcat, poll to see if the port is open, indicating that the NGRx is connected. If so, begin a download and validate that the files downloaded are good.
 # Poll port
 PORT=45500
-PORT_OPEN=`nc -z 127.0.0.1 ${PORT}`
-echo $PORT_OPEN
+nc -z 127.0.0.1 ${PORT}
+if [ $? -eq 0 ]
+then
+fi
