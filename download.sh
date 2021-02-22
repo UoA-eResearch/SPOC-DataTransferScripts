9ed8d5af35634d49f4c84b8ef73f5788028ce142
SPOC-sftp <c.seal@auckland.ac.nz>
2021-02-22 14:02:55 +1300

    Minor changes to make scripts work with systemd

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # download.sh - this script uses rsync over an ssh tunnel to copy the files from the remote server to the datacentre
 # Usage ./download.sh PORT_NUMBER where PORT_NUMBER is the port used for the reverse tunnel
-rsync --out-format="%n" --exclude-from=/srv/staging/downloads/eng.txt -avhe "ssh -p $1 -i ~/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/eng/* /srv/staging/downloads/eng/ #>> ${STAGINGDIR}/downloads_eng.txt
-rsync --out-format="%n" --exclude-from=/srv/staging/downloads/ins.txt -avhe "ssh -p $1 -i ~/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/ins/* /srv/staging/downloads/ins/ #>> ${STAGINIGDIR}/downloads_ins.txt
-rsync --out-format="%n" --exclude-from=/srv/staging/downloads/ngrx.txt -avhe "ssh -p $1 -i ~/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/ngrx/* /srv/staging/downloads/ngrx/ #>> ${STAGINIGDIR}/downloads_ngrx.txt
-rsync --out-format="%n" --exclude-from=/srv/staging/downloads/ngrx_raw.txt -avhe "ssh -p $1 -i ~/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/ngrx_raw/* /srv/staging/downloads/ngrx_raw/ #>> ${STAGINIGDIR}/downloads_ngrx_raw.txt
+rsync --out-format="%n" --exclude-from=/srv/staging/downloads/eng.txt -avhe "ssh -p $1 -i /home/ubuntu/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/eng/* /srv/staging/downloads/eng/ #>> ${STAGINGDIR}/downloads_eng.txt
+rsync --out-format="%n" --exclude-from=/srv/staging/downloads/ins.txt -avhe "ssh -p $1 -i /home/ubuntu/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/ins/* /srv/staging/downloads/ins/ #>> ${STAGINIGDIR}/downloads_ins.txt
+rsync --out-format="%n" --exclude-from=/srv/staging/downloads/ngrx.txt -avhe "ssh -p $1 -i /home/ubuntu/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/ngrx/* /srv/staging/downloads/ngrx/ #>> ${STAGINIGDIR}/downloads_ngrx.txt
+rsync --out-format="%n" --exclude-from=/srv/staging/downloads/ngrx_raw.txt -avhe "ssh -p $1 -i /home/ubuntu/.ssh/michigansoc" spoc@localhost:/mnt/int-ssd/download/ngrx_raw/* /srv/staging/downloads/ngrx_raw/ #>> ${STAGINIGDIR}/downloads_ngrx_raw.txt
 
--- poll.sh
+++ poll.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Using netcat, poll to see if the port is open, indicating that the NGRx is connected. If so, begin a download and validate that the files downloaded are good.
 # Poll port
-PORT=45500
+PORT=45503
 nc -z 127.0.0.1 ${PORT}
 if [ $? -eq 0 ] # port is open
 then
-    ./download.sh 45500 # Run the download script
+    ./download.sh ${PORT} # Run the download script
     # Run the upload script here
     ./validate_download.sh # Check the download and update the good download files so we don't re-download files unnecessariliy
 fi
